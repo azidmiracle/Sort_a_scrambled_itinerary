911d4b4292d2d006ed691d530ab30a77b290473c
Emely Diaz <45925710+azidmiracle@users.noreply.github.com>
2019-08-18 02:24:59 -0700

    Add files via upload

---
+++ Sort_a_scrambled_itinerary/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Sort_a_scrambled_itinerary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Browse = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelPrint = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_Browse
+            // 
+            this.btn_Browse.Location = new System.Drawing.Point(168, 12);
+            this.btn_Browse.Name = "btn_Browse";
+            this.btn_Browse.Size = new System.Drawing.Size(92, 32);
+            this.btn_Browse.TabIndex = 0;
+            this.btn_Browse.Text = "Sort";
+            this.btn_Browse.UseVisualStyleBackColor = true;
+            this.btn_Browse.Click += new System.EventHandler(this.Btn_Browse_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.labelPrint);
+            this.panel1.Location = new System.Drawing.Point(12, 69);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(386, 212);
+            this.panel1.TabIndex = 2;
+            // 
+            // labelPrint
+            // 
+            this.labelPrint.AutoSize = true;
+            this.labelPrint.Location = new System.Drawing.Point(3, 12);
+            this.labelPrint.Name = "labelPrint";
+            this.labelPrint.Size = new System.Drawing.Size(0, 13);
+            this.labelPrint.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 290);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_Browse);
+            this.Name = "Form1";
+            this.Text = "Sort Itinerary";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Browse;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelPrint;
+    }
+}
+
---
+++ Sort_a_scrambled_itinerary/Form1.cs
@@ -0,0 +1,138 @@
+﻿//This is an exercise problem from Google Kickstart.
+//I used C# and windows form
+//Problem
+//Once upon a day, Mary bought a one-way ticket from somewhere to somewhere with some flight transfers.
+
+//For example: SFO->DFW DFW->JFK JFK->MIA MIA->ORD.
+
+//Obviously, transfer flights at a city twice or more doesn't make any sense. So Mary will not do that.
+
+//Unfortunately, after she received the tickets, she messed up the tickets and she forgot the order of the ticket.
+
+//Help Mary rearrange the tickets to make the tickets in correct order.
+
+//Input
+//The first line contains the number of test cases T, after which T cases follow.
+//For each case, it starts with an integer N. There are N flight tickets follow.
+//Each of the next 2 lines contains the source and destination of a flight ticket.
+
+//Output
+//For each test case, output one line containing "Case #x: itinerary", where x is the test case number (starting from 1) and itinerary is sorted list of flight tickets which represents the actual itinerary.Each flight segment in the itinerary should be outputted as pair of source-destination airport codes.
+
+//Limits
+//1 ≤ T ≤ 100.
+//For each case, the input tickets are messed up from an entire itinerary bought by Mary.In other words, it's ensured can be recovered to a valid itinerary.
+
+//Small dataset
+//1 ≤ N ≤ 100.
+//Large dataset
+//1 ≤ N ≤ 104.
+//(The segment for second case in sample can be seen as below) MIA-ORD, DFW-JFK, SFO-DFW, JFK-MIA
+
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Sort_a_scrambled_itinerary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Browse_Click(object sender, EventArgs e)
+        {
+            int counter = 1;//initialize the counter for the flight
+            string line;//line in the text file to be read
+            int numberofcase;//number of test cases
+            int from_to;//the arrival location and destination location
+            string ALLtoPrintOut="";
+            string toPrintOut;
+
+            List<string> DatafromFile = new List<string>();//create a linkedlist for the data from textfile
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =
+            new System.IO.StreamReader(@"C:\Users\emely\source\repos\GoogleKickStart\Sort_a_scrambled_itinerary\database\C-small-practice.in");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                DatafromFile.Add(line);//add to linkelist
+            }
+
+            string[] result = DatafromFile.ToArray();//put the value into array
+
+            numberofcase = int.Parse(result[0]);//get the first value. This is for the number of test cases, T
+
+            //loop through the lines for T times
+            for (int i = 1; i < numberofcase + 1; i++)
+            {
+                from_to = int.Parse(result[counter])*2 ;//N flight tickets follow. Total places or points (start + destination)
+
+                //create Dictionary
+                Dictionary<string, string> Flights = new Dictionary<string, string>();
+
+                for (int x = counter + 1 ;x < counter + from_to; x += 2)
+                {
+                    Flights.Add(result[x], result[x+1]);//add to dictionary the key (departure point) and value (destination point) pair
+                }
+
+                string startpoint = StartPoint(Flights);//call this StartPoint function
+
+                toPrintOut = ("Case #" + i + ":" + PrintOut(Flights, startpoint) + "\n");//call the PrintOut function
+
+                ALLtoPrintOut = ALLtoPrintOut + toPrintOut ;
+
+                counter = counter + from_to + 1;
+
+            }
+
+            labelPrint.Text = ALLtoPrintOut;
+
+        }
+
+        public string StartPoint (Dictionary<string, string> Flights)//get the starting point.
+        {          
+            string key="";//initialize the starting
+
+            //loop through the key and find it in the value column.
+            foreach (KeyValuePair<string, string> ele1 in Flights)
+            {
+                key = ele1.Key;
+
+                if (Flights.ContainsValue(key))//if the key is found in destination column, then it is not the starting point.
+                {
+                    continue;
+                }
+
+                else//if the key is not found in the destination column, then this is the starting point.
+                {
+                    break;
+                }
+                    
+            }
+
+            return key;//return the starting point
+        }
+
+
+        public string PrintOut(Dictionary<string, string> Flights,string FirstPoint)// get the sorted pair
+        {
+            //get the first point
+
+            string Pair = "";
+
+            while (Flights.ContainsKey(FirstPoint) ==true)//loop through the dictionary until the final destination is reached
+            {
+                Pair= Pair + FirstPoint + "-" + Flights[FirstPoint] + " ";
+
+                FirstPoint = Flights[FirstPoint];
+            }
+
+            return Pair;
+        }
+
+    }
+}
